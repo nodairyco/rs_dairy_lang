111993152b086275362914f444288de015a33aa5
nodairyco <aniashvili.nodar@kiu.edu.ge>
2025-12-20 23:49:54 +0400

    token: added Token struct and everything associated with it

---
+++ src/token.rs
@@ -0,0 +1,106 @@
+#![allow(non_camel_case_types)]
+#![allow(unused)]
+
+use core::fmt;
+use std::{any::Any, fmt::write};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUNCT,
+    FUNCTIO,
+    FOR,
+    IF,
+    VICTIM,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    ME,
+    TRUE,
+    VAR,
+    VAL,
+    WHILE,
+
+    EOF,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    Nil,
+    Number(f64),
+    Str(String),
+    Bool(bool),
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexem: String,
+    pub line: u32,
+    pub literal: Option<Value>,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexem: String, line: u32, literal: Option<Value>) -> Token {
+        Token {
+            token_type: token_type,
+            lexem: lexem,
+            line: line,
+            literal: literal,
+        }
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Nil => write!(f, "nil"),
+            Value::Number(n) => write!(f, "{}", n),
+            Value::Str(str) => write!(f, "{}", str),
+            Value::Bool(bool) => write!(f, "{}", bool),
+        }
+    }
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let lit = match &self.literal {
+            Some(v) => format!("{}", v),
+            None => "VicTim".to_string(),
+        };
+        write!(f, "{:?} {} {}", self.token_type, self.lexem, lit)
+    }
+}
